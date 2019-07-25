37f95c72fc6e22fa53184aa89d8e4d585d033943
wpy <wpy@qq.com>
2019-07-25 10:54:14 +0800

    项目初始化

---
+++ pt-common/src/test/java/com/wpy/pt/common/PtCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wpy.pt.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PtCommonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ pt-dao/src/main/java/com/wpy/pt/dao/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wpy.pt.dao.mapper;
+
+import com.wpy.pt.dao.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String uid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String uid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
